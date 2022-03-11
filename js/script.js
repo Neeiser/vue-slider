41ebdea1c11980b532083fbd11af60df8b5255e2
Neeiser <stortini.study@gmail.com>
2022-03-11 16:24:37 +0100

    Structuring

--- js/script.js
+++ js/script.js
@@ -1,23 +1,33 @@
-const items = [
-    'img/01.jpg',
-    'img/02.jpg',
-    'img/03.jpg',
-    'img/04.jpg',
-    'img/05.jpg'
-];
+const root = new Vue({
+    el: '#root',
+    data: {
+        arrSlide: [
+            {
+                title: 'Svezia',
+                img: 'img/01.jpg',
+                text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
+            },
+            {
+                title: 'Svizzera',
+                img: 'img/02.jpg',
+                text: 'Lorem ipsum',
+            },
+            {
+                title: 'Gran Bretagna',
+                img: 'img/03.jpg',
+                text:  'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
+            },
+            {
+                title: 'Germania',
+                img: 'img/04.jpg',
+                text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
+            },
+            {
+                title: 'Paradise',
+                img: 'img/05.jpg',
+                text: 'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
+            },
+        ]
+    },
+});
 
-const title = [
-    'Svezia',
-    'Svizzera',
-    'Gran Bretagna',
-    'Germania',
-    'Paradise'
-]
-
-const text = [
-    'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
-    'Lorem ipsum',
-    'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
-    'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
-    'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
-]
